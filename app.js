e14fd3bf52a7439a0130176740843f53cc4548ab
Santiago <santicardonas777@gmail.com>
2023-02-27 19:02:07 -0500

    FunJs

---
+++ app.js
@@ -0,0 +1,19 @@
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+// console.log(c + b);
+
+
+// const saludo = c + d
+
+c = 'Hola de nuevo'
+
+
+
+
+
